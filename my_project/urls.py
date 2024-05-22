49ead20fbf873900c5fd1b3fe0ef7d24644ba581
jawad652923 <jawadashraf548@gmail.com>
2024-05-22 20:11:56 +0500

    test

--- my_project/urls.py
+++ my_project/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import (TokenObtainPairView,TokenRefreshView,)
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +26,5 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
+
+urlpatterns += staticfiles_urlpatterns()
